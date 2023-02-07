36ee0b825bd2ffad2955962681e1804ba4ab0afe
MaarkNassef <maark.nassef@gmail.com>
2023-02-07 21:31:44 +0200

    Playlist Downloader is available.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file, flash
-from youtube_downloader import VideoDownloader
+from youtube_downloader import VideoDownloader, PlaylistDownloader
 from io import BytesIO
+from zipfile import ZipFile
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Powerful Secret Key'
@@ -19,22 +20,53 @@ def index():
             return render_template('index.html')
         
         video_id = request.form["VideoLinkInput"].split('?v=')[1]
-        return redirect(url_for('download',vid_id = video_id))
+        return redirect(url_for('download_video',vid_id = video_id))
 
     if "PlaylistLinkInput" in request.form:
-        pass
+        if request.form["PlaylistLinkInput"] == '':
+            flash("Enter the URL.")
+            return render_template('index.html')
+        elif str(request.form["PlaylistLinkInput"]).find('playlist?list=') == -1:
+            flash("Enter a valid URL.")
+            return render_template('index.html')
+        
+        playlist_id = request.form["PlaylistLinkInput"].split('playlist?list=')[1]
+        return redirect(url_for('download_playlist',pl_id = playlist_id))
 
-@app.route('/download/<string:vid_id>', methods=['GET', 'POST'])
-def download(vid_id):
+@app.route('/download/video/<string:vid_id>', methods=['GET', 'POST'])
+def download_video(vid_id):
     try:
         video = VideoDownloader(f"""https://www.youtube.com/watch?v={vid_id}""")
     except:
         flash("Enter a valid URL.")
         return redirect(url_for('index'))
     if request.method == 'GET':
-        return render_template('download.html', title = video.title, thumbnail = video.thumbnail_url,
+        return render_template('download_video.html', title = video.title, thumbnail = video.thumbnail_url,
                                 resolutions = video.GetAvailableResolutions())
     return send_file(
         BytesIO(video.StreamToBuffer(request.form['SelectedResolution'])),
         as_attachment = True,
-        download_name=f'{video.title}.mp4')
+        download_name=f"""{video.title}.mp4""")
+
+@app.route('/download/playlist/<string:pl_id>', methods=['GET', 'POST'])
+def download_playlist(pl_id):
+    try:
+        playlist = PlaylistDownloader(f"""https://www.youtube.com/playlist?list={pl_id}""")
+        videos = playlist.GetVideos()
+    except:
+        flash("Enter a valid URL.")
+        return redirect(url_for('index'))
+    
+    if request.method == 'GET':
+        return render_template('download_playlist.html', title = playlist.title, videos = videos)
+    
+    # Create a ZipFile object in memory
+    in_memory = BytesIO()
+    with ZipFile(in_memory, 'w') as zip:
+        # Add multiple files to the zip file
+        for file in videos:
+            zip.writestr(f"""{file.title}.mp4""",BytesIO(file.StreamToBuffer()).getvalue())
+
+    # Set the zip file as the response
+    in_memory.seek(0)
+    return send_file(in_memory, download_name=f"""{playlist.title}.zip""", as_attachment=True)
--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,35 +1,57 @@
-from pytube import YouTube
+from pytube import YouTube, Playlist
 from io import BytesIO
 
 class VideoDownloader:
     def __init__(self, url: str) -> None:
         self.url = url
         self.youtube = YouTube(url)
-        self.streams = self.youtube.streams
-        self.title = self.streams.get_lowest_resolution().title
-        self.extension = self.streams.get_lowest_resolution().subtype
+        # self.streams = self.youtube.streams
+        self.title = self.youtube.title
         self.thumbnail_url = self.youtube.thumbnail_url
     
     def StreamToBuffer(self, resolution: str = '144p') -> bytes:
         buffer = BytesIO()
-        self.streams.filter(res=resolution, file_extension='mp4').first().stream_to_buffer(buffer)
+        if resolution == 'high':
+            self.youtube.streams.filter(file_extension='mp4').get_highest_resolution().stream_to_buffer(buffer)
+        elif resolution == 'low':
+            self.youtube.streams.filter(file_extension='mp4').get_lowest_resolution().stream_to_buffer(buffer)
+        else:
+            self.youtube.streams.filter(res=resolution, file_extension='mp4').first().stream_to_buffer(buffer)
         buffer.seek(0)
         return buffer.read()
 
     def GetAvailableResolutions(self) -> dict:
         res = set()
         result = {}
-        for i in self.streams.filter(file_extension='mp4'):
+        for i in self.youtube.streams.filter(file_extension='mp4'):
             if i.resolution:
                 res.add(i.resolution)
         for i in sorted(list(res)):
-            size = self.streams.filter(file_extension='mp4', res=i).first().filesize_mb
+            size = self.youtube.streams.filter(file_extension='mp4', res=i).first().filesize_mb
             result[i] = float("{:.1f}".format(size))
         return result        
 
 class PlaylistDownloader:
     def __init__(self, url) -> None:
-        pass
+        self.playlist = Playlist(url)
+        self.title = self.playlist.title
+        self.video_urls = self.playlist.video_urls
+
+    def GetVideosURL(self) -> list:
+        return list(self.video_urls)
+    
+    def GetVideosTitles(self) -> list[str]:
+        lst = []
+        for i in self.playlist.videos:
+            lst.append(i.title)
+        return lst
+
+    def GetVideos(self) -> list[VideoDownloader]:
+        return [VideoDownloader(v) for v in self.GetVideosURL()]
+
+# playlist = PlaylistDownloader('https://www.youtube.com/playlist?list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX')
+# for i in playlist.GetVideosURL():
+#     print(i)
 
 # video = VideoDownloader('https://www.youtube.com/watch?v=a8DM-tD9w2I')
 
